b67d6ca46031b1988ecb6a88e1583f467183da46
Karina Tiurina <rktiurina@gmail.com>
2018-01-19 11:37:36 +0300

    scope example

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
-   console.log("hello everybody"); 
+myApp.controller('mainController', function($scope) {
+    $scope.name = "Jane Doe";
+    $scope.occupation = "Coder";
+    
+    $scope.getname = function() {
+        return 'Name';
+    }
+    
+    $scope.getname();
+    console.log($scope);
 });
