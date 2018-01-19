225be13b5fc9b765751c279a449143ceeb7851ce
Karina Tiurina <rktiurina@gmail.com>
2018-01-19 12:26:57 +0300

    modules: ngMessages, ngResource

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log, $filter) {
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
     $scope.name = "john";
     $scope.formattedname = $filter('uppercase')($scope.name);
     
     $log.info($scope.name);
     $log.info($scope.formattedname);
+    
+    $log.log($resource);
+    
 });
 
