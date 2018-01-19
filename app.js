8ff4b414c85ddc5125fd2471e47f90200383da01
Karina Tiurina <rktiurina@gmail.com>
2018-01-19 12:47:09 +0300

    controller with array parameters

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log, $filter, $resource) {
-    $scope.name = "john";
-    $scope.formattedname = $filter('uppercase')($scope.name);
-    
-    $log.info($scope.name);
-    $log.info($scope.formattedname);
-    
-    $log.log($resource);
-    
-});
+myApp.controller('mainController', ['$scope', '$log', function(a, b) {
+   b.info(a);
+}]);
 
