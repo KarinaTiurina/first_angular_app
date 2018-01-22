da80507f42d2c685afeb3b6a71f15c7755512856
Karina Tiurina <rktiurina@gmail.com>
2018-01-22 11:02:13 +0300

    conflict resolved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-<<<<<<< HEAD
+
 myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
     $scope.name = 'Karina';
     
@@ -9,16 +9,5 @@ myApp.controller('mainController', ['$scope', '$timeout', function($scope, $time
     }, 3000);
  
 }]);
-=======
-myApp.controller('mainController', function($scope, $log, $filter, $resource) {
-    $scope.name = "john";
-    $scope.formattedname = $filter('uppercase')($scope.name);
-    
-    $log.info($scope.name);
-    $log.info($scope.formattedname);
-    
-    $log.log($resource);
-    
-});
->>>>>>> 4393146b9efbf77da7e5292e0f9eee4f86c814a5
+
 
